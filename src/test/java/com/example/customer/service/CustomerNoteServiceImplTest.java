22ab3b6de161e7d3a8d6c1da40878ffe7c7ac7f0
Robert Rowland <robertdavidrowland@gmail.com>
2018-04-13 17:29:24 +0100

    Service to constructor autowiring, remove useless tests

--- src/test/java/com/example/customer/service/CustomerNoteServiceImplTest.java
+++ src/test/java/com/example/customer/service/CustomerNoteServiceImplTest.java
@@ -2,53 +2,37 @@ package com.example.customer.service;
 
 import com.example.customer.model.CustomerNote;
 import com.example.customer.repository.CustomerNoteRepository;
-import com.example.customer.repository.CustomerRepositoryTest;
+import com.example.customer.repository.CustomerRepository;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.mockito.MockitoAnnotations;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
-@RunWith(SpringRunner.class)
 public class CustomerNoteServiceImplTest {
-
-    private static final Logger log = LoggerFactory.getLogger(CustomerRepositoryTest.class);
 
     private final CustomerNote note = new CustomerNote().setText("Lorem ipsum dollar");
 
-    @TestConfiguration
-    static class CustomerServiceImplTestContextConfiguration {
-
-        @Bean
-        public CustomerNoteService customerNoteService() {
-            return new CustomerNoteServiceImpl();
-        }
-    }
-
-    @Autowired
     private CustomerNoteService customerNoteService;
 
-    @MockBean
+    @Mock
+    private CustomerRepository customerRepository;
+
+    @Mock
     private CustomerNoteRepository customerNoteRepository;
 
     @Before
     public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        customerNoteService = new CustomerNoteServiceImpl(customerNoteRepository, customerRepository);
     }
 
     @Test
     public void getCustomerNoteByIdReturnsCustomerNote() {
-        log.debug("getCustomerNoteByIdReturnsCustomerNote");
-
         Mockito.when(customerNoteRepository.findById(note.getId())).thenReturn(Optional.of(note));
 
         Optional<CustomerNote> found = customerNoteService.getCustomerNoteById(note.getId());
--- src/test/java/com/example/customer/service/CustomerServiceImplTest.java
+++ src/test/java/com/example/customer/service/CustomerServiceImplTest.java
@@ -3,18 +3,11 @@ package com.example.customer.service;
 import com.example.customer.model.Customer;
 import com.example.customer.model.CustomerStatus;
 import com.example.customer.repository.CustomerRepository;
-import com.example.customer.repository.CustomerRepositoryTest;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.mockito.MockitoAnnotations;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,38 +15,25 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
-@RunWith(SpringRunner.class)
 public class CustomerServiceImplTest {
-
-    private static final Logger log = LoggerFactory.getLogger(CustomerRepositoryTest.class);
 
     private final Customer rob = new Customer().setId("rowlro01").setFirstName("Rob").setLastName("Rowland").setStatus(CustomerStatus.PROSPECTIVE);
     private final Customer andy = new Customer().setId("rowlan01").setFirstName("Andy").setLastName("Rowland").setStatus(CustomerStatus.CURRENT);
     private final Customer andy2 = new Customer().setId("imrian01").setFirstName("Andy").setLastName("Imrie").setStatus(CustomerStatus.NONACTIVE);
 
-    @TestConfiguration
-    static class CustomerServiceImplTestContextConfiguration {
-
-        @Bean
-        public CustomerService customerService() {
-            return new CustomerServiceImpl();
-        }
-    }
-
-    @Autowired
     private CustomerService customerService;
 
-    @MockBean
+    @Mock
     private CustomerRepository customerRepository;
 
     @Before
     public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        customerService = new CustomerServiceImpl(customerRepository);
     }
 
     @Test
     public void getCustomerByIdReturnsCustomer() {
-        log.debug("getCustomerByIdReturnsCustomer");
-
         Mockito.when(customerRepository.findById(rob.getId())).thenReturn(Optional.of(rob));
 
         Optional<Customer> found = customerService.getCustomerById(rob.getId());
@@ -64,8 +44,6 @@ public class CustomerServiceImplTest {
 
     @Test
     public void getCustomersByFirstNameReturnsCustomer() {
-        log.debug("getCustomersByFirstNameReturnsCustomer");
-
         List<Customer> customers = new ArrayList<>();
         customers.add(andy);
         customers.add(andy2);
@@ -80,8 +58,6 @@ public class CustomerServiceImplTest {
 
     @Test
     public void getCustomersByLastNameReturnsCustomer() {
-        log.debug("getCustomersByLastNameReturnsCustomer");
-
         List<Customer> customers = new ArrayList<>();
         customers.add(rob);
         customers.add(andy);
